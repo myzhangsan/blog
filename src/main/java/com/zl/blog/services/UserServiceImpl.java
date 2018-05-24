63714363c033582bff6f7bcecc32436677a35af8
zhangliang <290187924@qq.com>
2018-05-24 23:27:26 +0800

    补充

--- src/main/java/com/zl/blog/services/UserServiceImpl.java
+++ src/main/java/com/zl/blog/services/UserServiceImpl.java
@@ -1,5 +1,7 @@
 package com.zl.blog.services;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -9,21 +11,22 @@ import com.zl.blog.domain.User;
 import com.zl.blog.repository.UserRepository;
 
 /**
-* @author 作者 Zhang Liang:
-* @version 创建时间：2018年5月24日 下午10:57:13
-* 用户服务接口实现
-*/
+ * @author 作者 Zhang Liang:
+ * @version 创建时间：2018年5月24日 下午10:57:13 用户服务接口实现
+ */
 @Service
 public class UserServiceImpl implements UserService {
-	
+
 	@Autowired
-    private UserRepository userRepository;
+	private UserRepository userRepository;
+   //增删改要添加事务处理
+	@Transactional
 	@Override
 	public User saveOrUpateUser(User user) {
 		// TODO Auto-generated method stub
 		return userRepository.save(user);
 	}
-
+	@Transactional
 	@Override
 	public void removeUser(Long id) {
 		userRepository.deleteById(id);
@@ -37,13 +40,13 @@ public class UserServiceImpl implements UserService {
 
 	@Override
 	public Page<User> findUsersByNameLike(String name, Pageable pageable) {
-		// TODO Auto-generated method stub
-		return userRepository.findByNameLike(name,pageable);
+		name = "%" + name + "%";
+		return userRepository.findByNameLike(name, pageable);
 	}
 
 	@Override
 	public User findUserByUserNameLike(String userName) {
-		// TODO 
+		userName = "%" + userName + "%";
 		return userRepository.findUserByUserNameLike(userName);
 	}
 
